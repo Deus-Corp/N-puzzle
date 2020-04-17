c791683445c82a9d281583cc018afff2f63e6a5b
Remsko <pinoit.remi@gmail.com>
2020-04-17 22:32:23 +0200

    parsing: matrix dummy parsing

--- src/parsing.rs
+++ src/parsing.rs
@@ -1,6 +1,9 @@
+use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
+
+type Puzzle = Vec<Vec<u8>>;
 
 fn read_file(filename: &Path) -> io::Result<Vec<String>> {
 	let file = File::open(filename)?;
@@ -18,11 +21,23 @@ fn sanitize_comments(lines: &mut Vec<String>) -> Vec<String> {
 		.collect()
 }
 
-pub fn parsing(filename: &Path) -> io::Result<()> {
+fn parse_puzzle(lines: Vec<String>) -> Result<Puzzle, Box<dyn Error>> {
+	Ok(lines
+		.into_iter()
+		.skip(1)
+		.map(|line| {
+			line.split_whitespace()
+				.map(|number| number.parse().unwrap())
+				.collect()
+		})
+		.collect())
+}
+
+pub fn parsing(filename: &Path) -> Result<Puzzle, Box<dyn Error>> {
 	let mut file_lines_with_comments = read_file(filename)?;
 	let files_lines = sanitize_comments(&mut file_lines_with_comments);
-	println!("{:?}", files_lines);
-	Ok(())
+	let puzzle = parse_puzzle(files_lines)?;
+	Ok(puzzle)
 }
 
 #[cfg(test)]
@@ -38,6 +53,9 @@ mod tests {
 	#[test]
 	fn test_subject_1() {
 		let path = Path::new("./puzzles/subject-1.txt");
-		assert_eq!(parsing(path).unwrap(), ());
+		assert_eq!(
+			parsing(path).unwrap(),
+			vec![vec![3, 2, 6], vec![1, 4, 0], vec![8, 7, 5]]
+		);
 	}
 }
