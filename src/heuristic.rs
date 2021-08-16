0c00851624ef476e1bdc310317e8a56060e4770d
Remsko <pinoit.remi@gmail.com>
2021-08-16 23:31:36 +0200

    feat: linear conflicts start + tests

--- src/heuristic.rs
+++ src/heuristic.rs
@@ -1,7 +1,5 @@
 use super::puzzle::Puzzle;
 
-use std::collections::HashMap;
-
 pub enum Heuristic {
     Zero,
     HammingDistance,
@@ -23,36 +21,18 @@ fn hamming_distance(p1: &Puzzle, p2: &Puzzle) -> u32 {
     misplaced
 }
 
-// working only for idx + 1 (on flat) puzzle goal
-// fn manhattan_distance(p1: &Puzzle, _p2: &Puzzle) -> u32 {
-//     let mut distance: u32 = 0;
-//     for i in 0..p1.flat.len() {
-//         if p1.flat[i] == 0 {
-//             continue;
-//         }
-//         let goal_x = (p1.flat[i] - 1) as usize / p1.n;
-//         let goal_y = (p1.flat[i] - 1) as usize % p1.n;
-//         let x = i / p1.n;
-//         let y = i % p1.n;
-//         let dx = x as i16 - goal_x as i16;
-//         let dy = y as i16 - goal_y as i16;
-//         distance += (dx.abs() + dy.abs()) as u32;
-//     }
-//     distance
-// }
-
 fn manhattan_distance(p1: &Puzzle, p2: &Puzzle) -> u32 {
     let mut distance: u32 = 0;
-    let mut map = HashMap::new();
+    let mut map = vec![0; p1.flat.len()];
     for i in 0..p2.flat.len() {
-        map.insert(p2.flat[i], i);
+        map[p2.flat[i] as usize] = i;
     }
     for i in 0..p1.flat.len() {
         if p1.flat[i] == 0 {
             continue;
         }
 
-        let j = map.get(&p1.flat[i]).unwrap();
+        let j = map[p1.flat[i] as usize];
         let x1 = i / p1.n;
         let x2 = j / p1.n;
         let y1 = i % p1.n;
@@ -64,8 +44,59 @@ fn manhattan_distance(p1: &Puzzle, p2: &Puzzle) -> u32 {
     distance
 }
 
-fn linear_conflict(_p1: &Puzzle, _p2: &Puzzle) -> u32 {
-    0
+fn line_conflicts(p1: &Puzzle, p2: &Puzzle) -> u32 {
+    let mut conflicts: u32 = 0;
+    let mut map = vec![0; p1.flat.len()];
+    for i in 0..p2.flat.len() {
+        map[p2.flat[i] as usize] = i;
+    }
+    for row in 0..p1.n {
+        for col1 in 0..p1.n {
+            for col2 in (col1 + 1)..p1.n {
+                if p1.flat[col1 + row * p1.n] == 0
+                    || p1.flat[col2 + row * p1.n] == 0
+                {
+                    continue;
+                }
+                let goal_row1 =
+                    map[p1.flat[col1 + row * p1.n] as usize] / p1.n;
+                let goal_row2 =
+                    map[p1.flat[col2 + row * p1.n] as usize] / p1.n;
+                let goal_col1 =
+                    map[p1.flat[col1 + row * p1.n] as usize] % p1.n;
+                let goal_col2 =
+                    map[p1.flat[col2 + row * p1.n] as usize] % p1.n;
+                print!(
+                    "{} {} col:{} row:{} goal_row1:{} goal_row2:{} goal_col1:{} goal_col2:{}",
+                    p1.flat[col1 + row * p1.n],
+                    p1.flat[col2 + row * p1.n],
+                    col1,
+                    row,
+                    goal_row1,
+                    goal_row2,
+                    goal_col1,
+                    goal_col2,
+                );
+                if goal_row1 == goal_row2 && goal_col1 > goal_col2 {
+                    print!(" +1");
+                    conflicts += 1;
+                }
+                println!("");
+            }
+        }
+    }
+    print!("{:?}", p1);
+    conflicts
+}
+
+// for top to bottom, left to right
+// for col in 0..p1.n {
+//     for row in 0..p1.n {
+//         print!("{} ", p1.flat[col + row * p1.n]);
+//     }
+// }
+fn linear_conflict(p1: &Puzzle, p2: &Puzzle) -> u32 {
+    manhattan_distance(p1, p2) + line_conflicts(p1, p2) * 2
 }
 
 pub fn get_heuristic(
@@ -180,5 +211,29 @@ mod tests {
         );
 
         assert_eq!(manhattan_distance(&sum_4, &goal_snail), 4);
+    }
+
+    #[test]
+    fn test_linear_conflict() {
+        let sum_1 = Puzzle::from_matrix(
+            3,
+            vec![vec![0, 2, 1], vec![7, 4, 5], vec![6, 3, 8]],
+        );
+        let sum_3 = Puzzle::from_matrix(
+            3,
+            vec![vec![0, 2, 1], vec![5, 4, 3], vec![6, 7, 8]],
+        );
+        let sum_5 = Puzzle::from_matrix(
+            3,
+            vec![vec![2, 7, 0], vec![5, 4, 3], vec![8, 1, 6]],
+        );
+        let goal_classic = Puzzle::from_matrix(
+            3,
+            vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 8]],
+        );
+
+        assert_eq!(line_conflicts(&sum_1, &goal_classic), 1);
+        assert_eq!(line_conflicts(&sum_3, &goal_classic), 3);
+        assert_eq!(line_conflicts(&sum_5, &goal_classic), 5);
     }
 }
